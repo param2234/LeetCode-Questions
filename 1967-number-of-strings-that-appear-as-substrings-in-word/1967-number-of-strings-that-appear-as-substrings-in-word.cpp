90d445f5d3769d3f6dcfe838be983da35bbc6576
PARAMVEER SINGH DULLAT <paramdullat123@gmail.com>
2026-06-29 11:29:27 +0530

    Time: 0 ms (100%), Space: 11.6 MB (61.65%) - LeetHub

---
+++ 1967-number-of-strings-that-appear-as-substrings-in-word/1967-number-of-strings-that-appear-as-substrings-in-word.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int numOfStrings(vector<string>& patterns, string word) {
+        int count = 0;
+
+        for(auto &i:patterns){
+            
+            if(word.find(i) != string::npos){
+                count++;
+            }
+        }
+
+        return count;
+    }
+};
