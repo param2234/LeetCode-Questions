c35fe68613f8608a85bf3f3327d0a0652c84889f
PARAMVEER SINGH DULLAT <paramdullat123@gmail.com>
2026-07-17 12:41:23 +0530

    Time: 0 ms (100%), Space: 8.5 MB (81.39%) - LeetHub

--- 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
@@ -1,17 +1,6 @@
 class Solution {
 public:
     int gcdOfOddEvenSums(int n) {
-        
-        int even = n*(n+1);
-
-        int odd = n*n;
-
-        int div = 1;
-        for(int i = 1 ; i <= min(even,odd) ; i++){
-
-            if( even % i == 0 && odd % i == 0)div = i;
-        }
-
-        return div;
+        return n;
     }
 };
