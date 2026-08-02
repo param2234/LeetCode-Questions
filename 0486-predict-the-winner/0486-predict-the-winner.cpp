9c424c668476545fd30f8c9b490fff66c7182a29
PARAMVEER SINGH DULLAT <paramdullat123@gmail.com>
2026-08-02 10:53:26 +0530

    Time: 1 ms (46.53%), Space: 10.2 MB (32.41%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -3,25 +3,21 @@ public:
     bool predictTheWinner(vector<int>& nums) {
         
         int n = nums.size();
-        vector<vector<int>> dp(n, vector<int>(n, 0));
 
-        // Base case
-        for (int i = 0; i < n; i++)
+        vector<vector<int>>dp(n,vector<int>(n));
+
+        for(int i = 0;i<n;i++){
             dp[i][i] = nums[i];
+        }
 
-        // Fill by increasing length
-        for (int len = 2; len <= n; len++) {
-            for (int i = 0; i + len - 1 < n; i++) {
-                int j = i + len - 1;
+        for(int l = 2;l<=n;l++){
+            for(int i = 0;i + l -1 <n;i++){
+                int j = i + l - 1;
 
-                dp[i][j] = max(
-                    nums[i] - dp[i + 1][j],
-                    nums[j] - dp[i][j - 1]
-                );
+                dp[i][j] = max(nums[i] - dp[i+1][j],nums[j] - dp[i][j-1]);
             }
         }
 
-        return dp[0][n - 1] >= 0;
-
+        return dp[0][n-1] >= 0;
     }
 };
