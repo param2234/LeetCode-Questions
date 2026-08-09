ca0611cef2e1938ea3071f4120fd144c122529ec
PARAMVEER SINGH DULLAT <paramdullat123@gmail.com>
2026-08-09 12:19:25 +0530

    Time: 0 ms (100%), Space: 22.2 MB (8.83%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+
+        vector<int>arr(n+1,0);
+
+        for(int x:nums){
+            arr[x]++;
+        }
+
+        for(int i = 0;i<=n;i++){
+            if(arr[i] == 0)return i;
+        }
+
+        return -1;
+    }
+};
